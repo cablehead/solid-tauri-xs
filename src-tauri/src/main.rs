12eed7846ad9c71da59fc770059f77d72823f37b
Andy Gayton <andy@thecablelounge.com>
2024-09-12 22:18:58 -0400

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use xs::store::Store;
+
 fn main() {
+    let store = Store::spawn("./store".into());
     stacks2_lib::run()
 }
